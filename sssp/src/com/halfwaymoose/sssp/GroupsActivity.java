42ad427d29c4e1e4b720098cd0dd170e5acaf8d3
halfwaymoose <halfwaymoose@gmail.com>
2013-07-28 19:45:05 -0500

    Lots o' Stuff

---
+++ sssp/src/com/halfwaymoose/sssp/GroupsActivity.java
@@ -0,0 +1,14 @@
+package com.halfwaymoose.sssp;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class GroupsActivity extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_groups);
+	}
+	
+}
--- sssp/src/com/halfwaymoose/sssp/MainActivity.java
+++ sssp/src/com/halfwaymoose/sssp/MainActivity.java
@@ -1,22 +1,88 @@
 package com.halfwaymoose.sssp;
 
-import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+@SuppressLint("SimpleDateFormat")
+public class MainActivity extends Activity implements View.OnClickListener{
 
+	private TextView mTextDay;
+	private TextView mTextDate;
+	private TextView mTextTime;
+	private Button mButtonGroups;
+
+	private SimpleDateFormat mFormatDay;
+	private SimpleDateFormat mFormatDate;
+	private SimpleDateFormat mFormatTime;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	
+		setup();
+		
 	}
 
+	private void setup() {
+		
+		mTextDay = (TextView) findViewById(R.id.activity_main_txt_day);
+		
+		mTextDate = (TextView) findViewById(R.id.activity_main_txt_date);
+		
+		mTextTime = (TextView) findViewById(R.id.activity_main_txt_time);
+		
+		mButtonGroups = (Button) findViewById(R.id.activity_main_btn_groups);
+		mButtonGroups.setOnClickListener(this);
+		
+		mFormatDate = new SimpleDateFormat("MM/dd/yy");
+		mFormatDay = new SimpleDateFormat("MM/dd/yy");
+		mFormatTime = new SimpleDateFormat("hh:mm aa");
+		
+	}
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View view) {
+		
+		switch(view.getId()) {
+		
+		case R.id.activity_main_btn_groups:
+			
+			break;
+			
+		default:
+		
+		}
+		
 	}
-
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Calendar calendar;
+		Date date;
+		String format;
+		
+		calendar = Calendar.getInstance();
+		date = calendar.getTime();
+		
+		format = mFormatDay.format(date);
+		mTextDay.setText(format);
+		
+		format = mFormatDate.format(date);
+		mTextDate.setText(format);
+		
+		format = mFormatTime.format(date);
+		mTextTime.setText(format);
+		
+	}
+	
 }
