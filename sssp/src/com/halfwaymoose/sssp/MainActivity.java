b7ac5a1f94294f89257c075d5bbd611d01f7c05c
Halfwaymoose <Halfway.Moose@gmail.com>
2013-08-06 22:09:06 -0500

    Menu's

--- sssp/src/com/halfwaymoose/sssp/MainActivity.java
+++ sssp/src/com/halfwaymoose/sssp/MainActivity.java
@@ -6,14 +6,24 @@ import java.util.Date;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
 import android.widget.TextView;
 
 @SuppressLint("SimpleDateFormat")
 public class MainActivity extends Activity implements View.OnClickListener{
 
+	private static final int MENU_GROUPS = 0, MENU_TIMES = 1, MENU_CANCEL = 2;
+	
 	private TextView mTextDay;
 	private TextView mTextDate;
 	private TextView mTextTime;
@@ -44,7 +54,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		mButtonGroups.setOnClickListener(this);
 		
 		mFormatDate = new SimpleDateFormat("MM/dd/yy");
-		mFormatDay = new SimpleDateFormat("MM/dd/yy");
+		mFormatDay = new SimpleDateFormat("EEEE");
 		mFormatTime = new SimpleDateFormat("hh:mm aa");
 		
 	}
@@ -55,11 +65,89 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		switch(view.getId()) {
 		
 		case R.id.activity_main_btn_groups:
-			
+			showMenu(view);
 			break;
 			
 		default:
 		
+		}
+		
+	}
+	
+	@SuppressLint("NewApi")
+	public void showMenu(View view) {
+		PopupMenu popup;
+		MenuInflater inflater;
+		AlertDialog alertDialog;
+		AlertDialog.Builder alertBuilder;
+		
+		if (Build.VERSION.SDK_INT >= 11) {
+		
+			popup = new PopupMenu(this, view);
+			inflater = popup.getMenuInflater();
+			inflater.inflate(R.menu.main, popup.getMenu());
+			
+			popup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+				@Override
+				public boolean onMenuItemClick(MenuItem item) {
+					
+					switch (item.getItemId()) {
+					
+					case R.id.action_groups:
+						openGroups();
+						return true;
+						
+					case R.id.action_times:
+						openTimes();
+						return true;
+						
+					case R.id.action_cancel:
+						return false;
+					
+					default:
+						return false;
+						
+					}
+
+				}
+			});
+			
+			popup.show();
+		
+		} else {
+			
+			alertBuilder = new AlertDialog.Builder(this);
+			alertBuilder.setTitle("Main Menu");
+			
+			alertBuilder.setItems(R.array.main_menu_items, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					
+					switch (which) {
+					
+					case MENU_GROUPS:
+						openGroups();
+						break;
+						
+					case MENU_TIMES:
+						openTimes();
+						break;
+						
+					case MENU_CANCEL:
+						dialog.dismiss();
+						break;
+						
+					default:
+						dialog.dismiss();
+					
+					}
+					
+				}
+			});
+			
+			alertDialog = alertBuilder.create();
+			alertDialog.show();
+			
 		}
 		
 	}
@@ -82,6 +170,18 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		
 		format = mFormatTime.format(date);
 		mTextTime.setText(format);
+		
+	}
+	
+	private void openGroups() {
+		
+		startActivity(new Intent(this, GroupsActivity.class));
+		
+	}
+	
+	private void openTimes() {
+
+		startActivity(new Intent(this, TimesActivity.class));
 		
 	}
 	
